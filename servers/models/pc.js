7c4f2db37bf89f8a1dcc60d833fa69d2a9614f7e
newdeal <>
2021-05-05 16:44:29 +0900

    database.js 수정 -21.05.05

--- servers/models/pc.js
+++ servers/models/pc.js
@@ -1,13 +1,16 @@
-
 // const { Controller } = require('@nestjs/common');
 var db = require('./database');
 var ret = new Object();
 
+
 async function f(){
     var ret = await(db.getFirstQuery(1));
     console.log(ret);
-    console.log(ret.calls);
-    console.log(ret.calls[1].start + "to" + ret.calls[1].end)
+    const token = "HelloWorldToken";
+    var ret2 = await(db.createDBtable(token));
+    console.log(ret2);
+    var ret3 = await(db.deleteDBtable(token));
+    console.log(ret3);
 }
 
 f();
